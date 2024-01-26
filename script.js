86c970472c21383016f0ec1e7bfa4453ca9f8707
Renan Luis da Silva <renansilvaseo@gmail.com>
2024-01-26 13:27:29 -0300

    Adicionando a api para busca

---
+++ script.js
@@ -0,0 +1,63 @@
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((results) => displayResults(results));
+}
+
+function hidePlaylists() {
+    playlistContainer.classList.add("hidden");
+}
+
+function displayResults(results) {
+    hidePlaylists();
+    const artistImage = document.getElementById("artist-img");
+    const artistName = document.getElementById("artist-name");
+  
+    results.forEach((element) => {
+      artistImage.src = element.urlImg;
+      artistName.innerText = element.name;
+    });
+    resultArtist.classList.remove("hidden");
+  }
+  
+searchInput.addEventListener("input", function () {
+const searchTerm = searchInput.value.toLowerCase();
+if (searchTerm === "") {
+    resultArtist.classList.remove("hidden");
+    playlistContainer.classList.add("hidden");
+    return;
+}
+requestApi(searchTerm);
+});
+
+
+/*function displayResults (result){
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById("artist-name");
+    const artistImage = document.getElementById("artist-img");
+
+    result.forEach (element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    })
+
+    resultArtist.classList.remove("hidden");
+
+}*/
+
+/*document.addEventListener("input", function(){
+    const searchTerm = searchInput.value.toLowerCase();
+        if (searchTerm === ""){
+            resultPlaylist.classList.add("hidden");
+            resultArtist.classList.remove("hidden");
+            return;
+        }
+        requestApi(searchTerm);
+})*/
+
